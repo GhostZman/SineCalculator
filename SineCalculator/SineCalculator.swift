1c4057088ddc1b78f53853d4b3f4739e8ffc376e
Zachary Ives <zives@hawk.iit.edu>
2024-02-09 17:44:40 -0600

    Calculates sine given N and x via finite sum

--- SineCalculator/SineCalculator.swift
+++ SineCalculator/SineCalculator.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import Observation
+import SwiftUI
+
+@Observable class SineCalculator {
+    
+    func factorial(x: Int) -> Int{
+        
+        if x > 1 {
+            return x * factorial(x: (x-1))
+        }
+        return 1
+    }
+    
+    func calculateWithN(N: Int, x: Double) -> Double{
+        
+        var sine: Double = 0.0
+        for n in 1...N {
+            sine += pow(-1.0, Double(n-1)) * pow(x, Double(2*n-1)) / Double(factorial(x: (2*n - 1)))
+        }
+        return sine
+    }
+    
+    func calculateToSmallTerm(x: Double) -> Double {
+        var sine: Double = 0.0
+        
+        var n: Int = 1
+        var nextTerm: Double
+        repeat{
+            nextTerm = pow(-1.0, Double(n-1)) * pow(x, Double(2*n-1)) / Double(factorial(x: (2*n - 1)))
+            sine += nextTerm
+            n += 1
+        } while abs(nextTerm) > 1E-7 * sine
+        return sine
+    }
+}
--- SineCalculatorTests/SineCalculatorTests.swift
+++ SineCalculatorTests/SineCalculatorTests.swift
@@ -18,11 +18,9 @@ final class SineCalculatorTests: XCTestCase {
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        let mySine = SineCalculator()
+        
+        XCTAssertEqual(mySine.calculate(N: 5, x: 1), sin(1), accuracy: 1.0E-7, "Was not equal to this resolution.")
     }
 
     func testPerformanceExample() throws {
