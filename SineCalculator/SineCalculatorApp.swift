cbcfd7487e95c081a7f22372ac111e97376de64d
Zachary Ives <zives@hawk.iit.edu>
2024-02-09 17:17:43 -0600

    Initial Commit

---
+++ SineCalculator/SineCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SineCalculatorApp.swift
+//  SineCalculator
+//
+//  Created by Phys440Zachary on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SineCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
